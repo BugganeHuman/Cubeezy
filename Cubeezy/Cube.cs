411b2f068eb28bfa1ffb54c4ceb3d19eb7454e38
BugganeHuman <markizdelakarabas@gmail.com>
2026-09-18 17:07:14 +0400

    1v, it is working

--- Cubeezy/Cube.cs
+++ Cubeezy/Cube.cs
@@ -2,5 +2,12 @@
 
 public class Cube
 {
+    public static int RoleTheDie()
+    {
+        Random random = new Random();
+        int result = random.Next(1, 7);
+        return result;
+    }
+
 
 }
--- Cubeezy/Program.cs
+++ Cubeezy/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Cubeezy;
+
+Console.WriteLine("\nHello, Dude. I'm Cubeezy. I can role the die, do you want it?");
+int choice = 0;
+while (true)
+{
+    Console.Write("\nEnter '1' to role the die, and '0' to exit:  ");
+    try
+    {
+        choice = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (Exception exception)
+    {
+
+        Console.WriteLine("\nI can understand only integers like 1 or 0");
+        Console.WriteLine(exception);
+        continue;
+    }
+    if (choice == 1)
+    {
+        int result = Cube.RoleTheDie();
+        Console.WriteLine($"\n{result}");
+
+    }
+    else if (choice == 0)
+    {
+        break;
+    }
+}
+
+
+
+
+ 
+
+
