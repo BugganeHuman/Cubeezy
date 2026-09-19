68f02802add2e261d738223ecc30f29a6ddfcade
BugganeHuman <markizdelakarabas@gmail.com>
2026-09-19 15:34:25 +0400

    added future to roll 2 and more dice

--- Cubeezy/Program.cs
+++ Cubeezy/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Cubeezy;
 
 Console.WriteLine("\nHello, Dude. I'm Cubeezy. I can role the die, do you want it?");
-//int choice = 0;
 while (true)
 {
     Console.Write("\nEnter '1' to role the die, and '0' to exit:  ");
@@ -13,15 +12,30 @@ while (true)
         continue;
     }
    
-    if (choice == 1)
+    if (choice == 0 || choice < 0)
+    {
+        break;
+
+    }
+    else if (choice == 1)
+    // это сделано ибо один бросок кубика это просто утилитарная вещь, а 2 и более 
+    // кубика это уже игра, и каждый может ее определять и редактировать по своему
     {
         int result = Cube.RollTheDie();
         Console.WriteLine($"\n{result}");
-
     }
-    else if (choice == 0)
+    else
     {
-        break;
+        int result = 0;
+
+
+        for (int i = 0; i < choice; i++)
+        {   
+            int roll = Cube.RollTheDie();
+            result += roll;
+            Console.WriteLine($"\nroll {i + 1} = {roll}");
+        }
+        Console.WriteLine($"\n\nTotal Result = {result}");
     }
 }
 
